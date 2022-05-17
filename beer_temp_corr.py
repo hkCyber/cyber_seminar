a3dfeef341bb01a9392c8c6f8c5ad1367822cd61
Hiroto Katabami <bami@Hirotos-Air.bbrouter>
2022-05-17 13:06:19 +0900

    fix filtering data from Dec.24 to Dec.20 since there are near 100 beer expense data on those days

--- beer_temp_corr.py
+++ beer_temp_corr.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 import seaborn as sns
 
-import beer_temp_config.py as conf
+import beer_temp_config as conf
 
 # download　気象データ
 urlretrieve("https://raw.githubusercontent.com/kujirahand/book-mlearn-gyomu/master/src/ch2/tenki/kion10y.csv", "kion10y.csv")
@@ -16,7 +16,7 @@ df = pd.merge(df1, df2, on=['年', '月', '日'])
 
 # クリスマスから忘年会のシーズンとお盆のお供え物を買うシーズンを除外
 df = df.drop(df[(df['月'] == 8) & ((df['日'] >= 12) & (df['日'] <= 14))].index)
-df = df.drop(df[(df['月'] == 12) & (df['日'] >= 24)].index)
+df = df.drop(df[(df['月'] == 12) & (df['日'] >= 20)].index)
 
 print(df.corr())
 print(sns.heatmap(df.corr()))
